7ba7b410415109d1cbfa1f1f9ac4bbdbca855cb1
Varvara <lapinavr@gmail.com>
2024-04-24 20:31:09 +0700

    Удален ненужный файл

--- src/js/helpers.js
+++
@@ -1,7 +0,0 @@
-export default (feed, item) => ({
-  feed,
-  title: item.querySelector('title')?.textContent || 'Title',
-  description: item.querySelector('description')?.textContent ?? 'Description',
-  link: item.querySelector('link')?.textContent || 'Link',
-  publicationDate: item.querySelector('pubDate')?.textContent || 'Publication date',
-});
